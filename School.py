18636e46227ac12b8535fd887297d2d4c17278a9
Jerp010 <jeffmarquez2006@gmail.com>
2025-04-12 10:39:10 +0800

    Final

--- School.py
+++ School.py
@@ -14,30 +14,28 @@ class School(ABC):
         total = sum(self._students.values())
         return total / len(self._students)
 
-    @staticmethod
-    def gpa_scale():  # Static Method
-        return "GPA is out of 4.0 scale"
-
     @classmethod
-    def from_list(cls, name, student_list):  # Class Method
+    def from_list(cls, name, student_list):
         students = {name: grade for name, grade in student_list}
         return cls(name, students)
 
-# Inheritance
+# Inheritance for School 1
 class SchoolOne(School):
     def display_results(self):  # Polymorphism
         print(f"--- {self._name} ---")
         for student, grade in self._students.items():
             print(f"{student}: {grade}")
         print("Average Grade:", self.get_average())
+        print("GPA:", round(self.get_average() / 25, 2))
 
-# Inheritance
+# Inheritance for school 2
 class SchoolTwo(School):
     def display_results(self):  # Polymorphism
         print(f"--- {self._name} ---")
         for student, grade in self._students.items():
             print(f"{student}: {grade}")
-        print("GPA (out of 4):", round(self.get_average() / 25, 2))
+        print("Average Grade:", self.get_average())
+        print("GPA:", round(self.get_average() / 25, 2))
 
 
 def main():
@@ -48,11 +46,10 @@ def main():
     school1 = SchoolOne.from_list("School One", list_one)
     school2 = SchoolTwo.from_list("School Two", list_two)
 
-    school1.display_results()  # Polymorphism
-    print("---")
-    school2.display_results()  # Polymorphism
+    school1.display_results()
+    print("")
+    school2.display_results()
 
-    print(School.gpa_scale())  # Static Method
 
 
 if __name__ == "__main__":
